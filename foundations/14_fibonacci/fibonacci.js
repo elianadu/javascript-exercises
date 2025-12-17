52050f8efa25a83bf340ef63d128ab02d5b8772c
elianadu <148028744+elianadu@users.noreply.github.com>
2025-12-17 07:01:20 -0500

    fix: complete exercises 14, 15, 16

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  if (index < 0) {
+    return "OOPS";
+  }
 
+  index = Number(index);
+
+  if (index === 0) return 0;
+  if (index === 1 || index === 2) return 1;
+  prevNum0 = 1;
+  prevNum1 = 1;
+  thisNum = 2;
+
+  for (let i = 3; i < index; i++) {
+    [prevNum0, prevNum1, thisNum] = [prevNum1, thisNum, prevNum1 + thisNum];
+  }
+  return thisNum;
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArr) {
+    sortedArr = peopleArr.sort((a, b) => {
+        if (a.yearOfDeath == undefined) a.yearOfDeath = (new Date).getFullYear();
+        if (b.yearOfDeath == undefined) b.yearOfDeath = (new Date).getFullYear();
+        aAge = a.yearOfDeath - a.yearOfBirth;
+        bAge = b.yearOfDeath - b.yearOfBirth;
+        return bAge - aAge;
+    });
+    return sortedArr[0];
 };
 
 // Do not edit below this line
